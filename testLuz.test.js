32eb9fbd0e489dd453db383228072c2f24c6ab8b
unknown <mateomigone01@gmail.com>
2022-10-25 20:40:52 -0300

    TDD iteration

--- testLuz.test.js
+++ testLuz.test.js
@@ -4,3 +4,9 @@ test("crear luz", () => {
     const luz = new Luz();
     expect (luz.consultarEstado()).toBe("apagada");
 });
+
+test("encender luz", () => {
+    const luz = new Luz();
+    luz.encender();
+    expect (luz.consultarEstado()).toBe("apagada");
+});
