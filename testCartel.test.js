100901298587b0eef48b889f16e61317270f8336
unknown <mateomigone01@gmail.com>
2022-10-25 22:21:37 -0300

    TDD iteration

--- testCartel.test.js
+++ testCartel.test.js
@@ -1,6 +1,18 @@
 const Cartel = require('./Cartel');
 
-test("crear cartel", () => {
-    const cartel = new Cartel();
-    expect (cartel.cantidadLucesEncendidas()).toBe(0);
+var cartel;
+beforeEach(function () {
+    cartel = new Cartel();
 });
+
+test("crear cartel", () => {
+    expect (cartel.cantidadLucesEncendidas()).toBe(0);
+    expect (cartel.cantidadLuces).toBe(10000);
+});
+
+// test("encender luces del rectangulo con vertice superior izquierdo (0,0) e inferior derecho (2,2)", () => {
+//     cartel.encenderLuces([0,0],[2,2]);
+//     expect (cartel.estadoRectangulo([0,0],[2,2])).toBe("encendido");
+// });
+
+
