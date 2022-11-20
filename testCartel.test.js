d93a719c0e823f54b93d0c45787759d9f2ccc53e
unknown <mateomigone01@gmail.com>
2022-11-19 22:05:28 -0300

    TDD iteration

--- testCartel.test.js
+++ testCartel.test.js
@@ -3,18 +3,17 @@ const Luz = require('./Luz');
 
 var cartel;
 beforeEach(function () {
-    cartel = new Cartel(2,2);
+    cartel = new Cartel(100,100);
 });
 
 test("crear cartel", () => {
     expect (cartel.cantidadLucesEncendidas()).toBe(0);
-    expect (cartel.cantidadLuces()).toBe(4);
-    expect (cartel.cantFilas).toBe(2);
-    expect (cartel.cantColumnas).toBe(2);
+    expect (cartel.cantidadLuces()).toBe(10000);
+    expect (cartel.cantFilas).toBe(100);
+    expect (cartel.cantColumnas).toBe(100);
     expect (Luz.prototype.isPrototypeOf(cartel.matriz[0][0])).toBe(true);
-    expect (Luz.prototype.isPrototypeOf(cartel.matriz[0][1])).toBe(true);
-    expect (Luz.prototype.isPrototypeOf(cartel.matriz[1][0])).toBe(true);
-    expect (Luz.prototype.isPrototypeOf(cartel.matriz[1][1])).toBe(true);
+    expect (Luz.prototype.isPrototypeOf(cartel.matriz[99][99])).toBe(true);
+    
 });
 
 // test("encender luces del rectangulo con vertice superior izquierdo (0,0) e inferior derecho (2,2)", () => {
