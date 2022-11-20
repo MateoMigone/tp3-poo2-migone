cd390b9c5708767b76e245c59a6e8afa1f40bca7
unknown <mateomigone01@gmail.com>
2022-11-19 21:41:15 -0300

    TDD iteration

--- Cartel.js
+++ Cartel.js
@@ -1,5 +1,14 @@
+const Luz = require("./Luz");
+    
 function Cartel(filas,columnas){
+    this.cantFilas = filas;
+    this.cantColumnas = columnas;
     this.matriz = [];
+    for (let i=0; i<this.cantFilas; i++){
+        for (let j=0; j<this.cantColumnas; j++){
+            this.matriz[i][j] = new Luz();
+        }
+    }
 
     this.cantidadFilas = function (){
         return 2
@@ -14,7 +23,7 @@ function Cartel(filas,columnas){
     }
 
     this.cantidadLuces = function(){
-        return 10000;
+        return 4;
     }
 }
 module.exports = Cartel;
--- testCartel.test.js
+++ testCartel.test.js
@@ -8,13 +8,13 @@ beforeEach(function () {
 
 test("crear cartel", () => {
     expect (cartel.cantidadLucesEncendidas()).toBe(0);
-    expect (cartel.cantidadLuces()).toBe(10000);
+    expect (cartel.cantidadLuces()).toBe(4);
     expect (cartel.cantidadFilas()).toBe(2);
     expect (cartel.cantidadColumnas()).toBe(2);
-    expect (Luz.prototype.isPrototyprOf(cartel.matriz[0][0])).toBe(true);
-    expect (Luz.prototype.isPrototyprOf(cartel.matriz[0][1])).toBe(true);
-    expect (Luz.prototype.isPrototyprOf(cartel.matriz[1][0])).toBe(true);
-    expect (Luz.prototype.isPrototyprOf(cartel.matriz[1][1])).toBe(true);
+    expect (Luz.prototype.isPrototypeOf(cartel.matriz[0][0])).toBe(true);
+    expect (Luz.prototype.isPrototypeOf(cartel.matriz[0][1])).toBe(true);
+    expect (Luz.prototype.isPrototypeOf(cartel.matriz[1][0])).toBe(true);
+    expect (Luz.prototype.isPrototypeOf(cartel.matriz[1][1])).toBe(true);
 });
 
 // test("encender luces del rectangulo con vertice superior izquierdo (0,0) e inferior derecho (2,2)", () => {
