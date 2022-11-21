2f9b1db58d35096ca6d5c008045696734bc9683f
unknown <mateomigone01@gmail.com>
2022-11-20 22:58:30 -0300

    TDD iteration

--- testLuz.test.js
+++ testLuz.test.js
@@ -74,6 +74,7 @@ test("encender luz si ya esta la intensidad al maximo", () => {
         luz.cambiar();
         luz.encender();
     }).toThrow("No se puede aumentar la intensidad a mas que 10");
+    
 });
 
 
